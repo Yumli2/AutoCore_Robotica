4bc01c14992eb76b5a69a693643cfb1a0ab10335
Yuri Lima <y.m.lima19@gmail.com>
2015-05-10 12:50:34 -0300

    Refatorado

--- Projetos Particulares/Projeto_2_Ponte_0/Projeto_2_Ponte_0.ino
+++ Projetos Particulares/Projeto_2_Ponte_0/Projeto_2_Ponte_0.ino
@@ -1,258 +1,223 @@
-//Projeto_2_Ponte_0.ino
-
-// definição dos pinos dos segmentos
-int a = 2;
-int b = 3;
-int c = 4;
-int d = 5;
-int e = 6;
-int f = 7;
-int g = 8;
-
-
-//definição dos pinos dos dígitos
-int di1 = 9;
-int di2 = 10;
-int di3 = 11;
-int di4 = 12;
-
-// outras definições
-long n ;
-long m ;
-int y= 100;
-int del2 = 2200;
-int x = 100;
-int del = 2200;
-int z = 1;
-int w = 1;
-int loopcount = 0;
-int tempo = 0;
-int tempo2 = 0;
-int testch;
-
-int ch2,ch1;
+//Projeto Controle de Fluxo para Ponte em Ibutirama Bahia
+//Empresa: Autocore Robótica
+//Desenvolvedor: Yuri Lima
+//Data de inicio: 25/04/2015
+//=========================================================================================================================
+//=========================================================================================================================
+//Definições dos pinos Display de 7 segmentos
+#define a 2 
+#define b 3
+#define cc 4 //usando apenas 'c' deu erro, não entendi o motivo!
+#define d 5
+#define e 6
+#define f 7
+#define g 8
+//=========================================================================================================================
+//definição dos dígitos
+#define d1 9
+#define d2 10
+#define d3 11
+#define d4 12
+//=========================================================================================================================
+//Definição das Chaves
+#define sensor1 50
+#define sensor2 52
+//=========================================================================================================================
+//Outras definições
+#define del 2000//regula o tempo
+int x = 200; //Define a velocidade da contagem
+short int ch1,ch2;
+short int n=0,m=0,tempo=0,tempo2=0,i=0,j=0,h=1,tempoInterno=0;
+boolean flag= true;
+//=========================================================================================================================
+//=========================================================================================================================
 // inicialização do Arduino
-void setup()
-{
+void setup(){
   Serial.begin(9600);
-  pinMode(di1, OUTPUT);
-  pinMode(di2, OUTPUT);
-  pinMode(di3, OUTPUT);
-  pinMode(di4, OUTPUT);
-  pinMode(a, OUTPUT);
-  pinMode(b, OUTPUT);
-  pinMode(c, OUTPUT);
-  pinMode(d, OUTPUT);
-  pinMode(e, OUTPUT);
-  pinMode(f, OUTPUT);
-  pinMode(g, OUTPUT);
-  pinMode(A0,OUTPUT);
-  pinMode(A1,OUTPUT);
-  pinMode(A4, INPUT);
-  pinMode(A5, INPUT);
-  
-  
- while (testch==0){
-  Serial.println("sd");
-   ch1 = digitalRead(A4); // teste logico das chaves
-   ch2 = digitalRead(A5);
-   if (ch1==HIGH){
-      z=1;
-     chave1();
-    }
-    else if(ch2==HIGH){
-      w=1;
-      chave2();
-    }
-    else{
-      w=0;
-      z=0;
-    }
-    testch = w+z;
+  for(short int v=2;v<=12;v++){
+    pinMode(v, OUTPUT);//Coloca os pinos 2 a 12 em OUTPUT
   }
-  Serial.println("sd");
+  pinMode(sensor1, INPUT_PULLUP);
+  pinMode(sensor2, INPUT_PULLUP);
 }
-  void chave1(){
-while (tempo==0)
-{
-  ch1 = digitalRead(A4);
-  if (ch1==HIGH)
-   {
-    n= n+100; // Incrementa n
-  delay(1000);
- }
-   else
-   {tempo = n;
-     chave2();}
-   
-}
-  }
-void chave2(){
-  
-while (tempo2==0)
-{
-  int ch2 = digitalRead(A5);
-  if (ch2 == HIGH)
-    {
-  delay(1000);  
-    m=m+100; // Incrementa n
-    }  
-  else
-  {tempo2 = m;
-    chave1();}
-}
-}
-
-
+//=========================================================================================================================
+//=========================================================================================================================
 // loop principal do programa
 void loop(){
-  //Serial.println(ch1);
-   //Serial.println(ch2);
-	if (n<=0){//100<=0? n--
-    	digitalWrite(A1,HIGH);
-    }
-    else{
-    	delay(3);
-     	n--; // decrementa n
-    }
-    if (m<=0){
-     digitalWrite(A0,HIGH);
-    }
-    else{
-    	delay(3);
-     	m--; // decrementa n
-    }
-     
-    if (digitalRead(A4) == LOW){ // verifica se botão foi pressionado
-  		n = tempo; // se sim zera o contador
-    	digitalWrite(A1,LOW);
- 	}
-   if (digitalRead(A5) == LOW){ // verifica se botão foi pressionado
-  		m = tempo2; // se sim zera o contador
-    	digitalWrite(A0,LOW);
-  	}
-  
-  
-  	if ( digitalRead(A5) == HIGH){ // verifica se botão foi pressionado
-  		clearLEDs(); // apaga todos os segmentos
-    	pickDigit(1); // seleciona dígito 1
-    	pickNumber((m/y/10)%10); // gera o número a ser mostrado
-                             // baseado em n e x
-    	delayMicroseconds(del2); // delay em microsegundos
-  
-  		clearLEDs();
-  		pickDigit(2);
-  		pickNumber((m/y%10));
-  		delayMicroseconds(del2);
-  	}
-  	else{
-   		if ( digitalRead(A4) == HIGH){ // verifica se botão foi pressionado
-  			clearLEDs();
-  			pickDigit(3);
-  			//dispDec(3);
-  			pickNumber((n/x/10)%10);
-  			delayMicroseconds(del);
-  
-  			clearLEDs();
-  			pickDigit(4);
-  			pickNumber(n/x%10);
-  			delayMicroseconds(del);
-  		}
-  		else{
-  			digitalWrite(di1, HIGH);// valor HIGH no pino desabilita o dígito
- 			digitalWrite(di2, HIGH);
-  			digitalWrite(di3, HIGH);
-  			digitalWrite(di4, HIGH);  
-		}
-	}
+    Sentido_AB();
+    Sentido_BA();
 }
+//=========================================================================================================================
+//=========================================================================================================================
+void Sentido_AB(){
+   if (digitalRead(sensor1)==HIGH && j==0){ //O carro passou pelo sensor
+        i=0;
+        chave1();
+  }   
+  if (digitalRead(sensor1)==HIGH && j==1){ //Quando o segundo ou demais carros passa pelo sensor
+  tempoInterno=0;  
 
+   while((tempo>0) || (flag==true)){ 
+      while(digitalRead(sensor2)==LOW ){
+        clearLEDs();
+        pickDigit(1);
+        pickNumber(tempo/x%10);
+        delayMicroseconds(del);
+      
+        clearLEDs();
+        pickDigit(2);
+        pickNumber((tempo/x/10)%10);
+        delayMicroseconds(del);
+        if(tempo>0){
+          tempo--; // incrementa n
+        }
+        tempoInterno+=1; // Tem que ser tempoInterno +1 por que Tempo-- vai decrementar o tempo 1000 por exemplo de um a um
+        if(digitalRead(sensor2)!=LOW){
+          tempo=0;
+          goto para;
+        }
+      }
+      para:
+      flag=false;
+    } 
+    Serial.print("tempo: ");
+    Serial.println(tempo);
+    Serial.print("tempo Interno: ");
+    Serial.println(tempoInterno);
+    tempo= tempoInterno;
+    ch1=LOW;
+    
+  }
+}
+//=========================================================================================================================
+//=========================================================================================================================
+// teste logico das chaves  
+void chave1(){
+  while (digitalRead(sensor2)==LOW){
+        Serial.print(n);
+        Serial.print(" - ");
+        Serial.print(i);
+        Serial.println(" Segundos");
+        i++;
+        n = n+200; // Incrementa n
+        delay(1000); //Segundos
+      }
+      ch1=LOW;
+      j=1;
+      tempo += n;
+        //chave2();
+}
+void chave1(){
+  while (digitalRead(sensor2)==LOW){
+        Serial.print(n);
+        Serial.print(" - ");
+        Serial.print(i);
+        Serial.println(" Segundos");
+        i++;
+        n = n+200; // Incrementa n
+        delay(1000); //Segundos
+      }
+      ch1=LOW;
+      j=1;
+      tempo += n;
+        //chave2();
+}
+//=========================================================================================================================
+//=========================================================================================================================
 // subrotina para multiplexar os dígitos do display
 void pickDigit(int x){
-	digitalWrite(di1, HIGH);// valor HIGH no pino desabilita o dígito
-  	digitalWrite(di2, HIGH);
-  	digitalWrite(di3, HIGH);
-  	digitalWrite(di4, HIGH);
+  digitalWrite(d1, HIGH);// valor HIGH no pino desabilita o dígito
+  digitalWrite(d2, HIGH);
+  digitalWrite(d3, HIGH);
+  digitalWrite(d4, HIGH);
   
-	switch(x){ // cria uma estrutura switch, tendo como entrada
-        // o número do display a ser habilitado
-  	  	case 1: 
-    	digitalWrite(di1, LOW); // colocando LOW no pino, estamos habilitando o acendimento dos segmentos
-        break;   // executou? sai da estutura                  
-        case 2: 
-      	digitalWrite(di2, LOW); 
-      	break;
-    	case 3: 
-      	digitalWrite(di3, LOW); 
-     	break;
-    	default: 
-      	digitalWrite(di4, LOW); 
-      	break;
-  	}
+  switch(x){ // cria uma estrutura switch, tendo como entrada
+            // o número do display a ser habilitado
+    case 1: 
+      digitalWrite(d1, LOW); // colocando LOW no pino, estamos habilitando
+                             // o acendimento dos segmentos
+      break; // executou? sai da estutura
+    case 2: 
+      digitalWrite(d2, LOW); 
+      break;
+    case 3: 
+      digitalWrite(d3, LOW); 
+      break;
+    default: 
+      digitalWrite(d4, LOW); 
+      break;
+  }
 }
-
+//=========================================================================================================================
+//=========================================================================================================================
 // subrotina para acender um determinado número no display
-void pickNumber(int x)
-{
-  switch(x) // cria uma estrutura switch para seleção
-  {
+int pickNumber(int x){
+  switch(x){ // cria uma estrutura switch para seleção
     default: 
       zero(); // chama rotina para apresentar número 0 no display 
+      return 0;
       break;  // executou? retorna da subrotina
     case 1: 
-      one(); 
+      one();
+      return 1; 
       break;
     case 2: 
-      two(); 
+      two();
+      return 2; 
       break;
     case 3: 
       three(); 
+      return 3;
       break;
     case 4: 
-      four(); 
+      four();
+      return 4; 
       break;
     case 5: 
-      five(); 
+      five();
+      return 5; 
       break;
     case 6: 
       six(); 
+      return 6;
       break;
     case 7: 
-      seven(); 
+      seven();
+      return 7; 
       break;
     case 8: 
-      eight(); 
+      eight();
+      return 8; 
       break;
     case 9: 
       nine(); 
+      return 9;
       break;
   }
 }
 
-//void dispDec(int x)
-//{
-  //digitalWrite(p, HIGH);
-//}
-
+//=========================================================================================================================
+//=========================================================================================================================
 // subrotina para apagar todos os segmentos
 void clearLEDs()
 {
   digitalWrite(a, LOW); // valor low no pino, apaga segmento
   digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
+  digitalWrite(cc, LOW);
   digitalWrite(d, LOW);
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
   digitalWrite(g, LOW);
   //digitalWrite(p, LOW);
 }
-
+//=========================================================================================================================
+//=========================================================================================================================
 // mostra número sero no display
 void zero()
 {
   digitalWrite(a, HIGH); // valor HIGH no pino, acende segmento
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, HIGH);
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
@@ -263,7 +228,7 @@ void one()
 {
   digitalWrite(a, LOW);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, LOW);
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
@@ -274,7 +239,7 @@ void two()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
-  digitalWrite(c, LOW);
+  digitalWrite(cc, LOW);
   digitalWrite(d, HIGH);
   digitalWrite(e, HIGH);
   digitalWrite(f, LOW);
@@ -285,7 +250,7 @@ void three()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, HIGH);
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
@@ -296,7 +261,7 @@ void four()
 {
   digitalWrite(a, LOW);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, LOW);
   digitalWrite(e, LOW);
   digitalWrite(f, HIGH);
@@ -307,7 +272,7 @@ void five()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, LOW);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, HIGH);
   digitalWrite(e, LOW);
   digitalWrite(f, HIGH);
@@ -318,7 +283,7 @@ void six()
 {
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, HIGH);
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
@@ -329,7 +294,7 @@ void seven()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, LOW);
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
@@ -340,7 +305,7 @@ void eight()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, HIGH);
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
@@ -351,7 +316,7 @@ void nine()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, LOW);
   digitalWrite(e, LOW);
   digitalWrite(f, HIGH);
--- Projetos Particulares/TESTESSSS/TESTESSSS.ino
+++ Projetos Particulares/TESTESSSS/TESTESSSS.ino
@@ -1,90 +1,138 @@
-// definição dos pinos dos segmentos
-int a = 2;
-int b = 3;
-int c = 4;
-int d = 5;
-int e = 6;
-int f = 7;
-int g = 8;
-//int p = 8;
-
-//definição dos pinos dos dígitos
-int d1 = 9;
-int d2 = 10;
-int d3 = 11;
-int d4 = 12;
-boolean flag= true; 
-// outras definições
-int tempo1; //20000==99
-int x = 200; 
-int del = 2000; //regula o tempo
-
+//Projeto Controle de Fluxo para Ponte em Ibutirama Bahia
+//Empresa: Autocore Robótica
+//Desenvolvedor: Yuri Lima
+//Data de inicio: 25/04/2015
+//=========================================================================================================================
+//=========================================================================================================================
+//Definições dos pinos Display de 7 segmentos
+#define a 2 
+#define b 3
+#define cc 4 //usando apenas 'c' deu erro, não entendi o motivo!
+#define d 5
+#define e 6
+#define f 7
+#define g 8
+//=========================================================================================================================
+//definição dos dígitos
+#define d1 9
+#define d2 10
+#define d3 11
+#define d4 12
+//=========================================================================================================================
+//Definição das Chaves
+#define sensor1 50
+#define sensor2 52
+//=========================================================================================================================
+//Outras definições
+#define del 2000//regula o tempo
+int x = 200; //Define a velocidade da contagem
+short int ch1,ch2;
+short int n=0,m=0,tempo=0,tempo2=0,i=0,j=0,h=1,tempoInterno=0;
+boolean flag= true;
+//=========================================================================================================================
+//=========================================================================================================================
 // inicialização do Arduino
-void setup()
-{
-	Serial.begin(9600);
-  	pinMode(d1, OUTPUT);
-  	pinMode(d2, OUTPUT);
-  	pinMode(d3, OUTPUT);
-  	pinMode(d4, OUTPUT);
-  	pinMode(a, OUTPUT);
-  	pinMode(b, OUTPUT);
-  	pinMode(c, OUTPUT);
-  	pinMode(d, OUTPUT);
-  	pinMode(e, OUTPUT);
-  	pinMode(f, OUTPUT);
-  	pinMode(g, OUTPUT);
-  	//pinMode(p, OUTPUT);
+void setup(){
+  Serial.begin(9600);
+  for(short int v=2;v<=12;v++){
+    pinMode(v, OUTPUT);//Coloca os pinos 2 a 12 em OUTPUT
+  }
+  pinMode(sensor1, INPUT_PULLUP);
+  pinMode(sensor2, INPUT_PULLUP);
 }
-
+//=========================================================================================================================
+//=========================================================================================================================
 // loop principal do programa
-void loop()
-{
-	
-		long n=tempo1();
-    	clearLEDs();
-  	  	pickDigit(1);
- 	  	pickNumber(n/x%10);
-  		delayMicroseconds(del);
-
- 		clearLEDs();
-  		pickDigit(2);
-  		pickNumber((n/x/10)%10);
-  		delayMicroseconds(del);
-  		n--; // incrementa n
-  		if (digitalRead(A4) == HIGH){ // verifica se botão foi pressionado
-    		Serial.println(digitalRead(A4));
-   			 n = 20000; // se sim zera o contador
-  		}
-  		
+void loop(){
+    Sentido_AB();
+    Sentido_BA();
 }
+//=========================================================================================================================
+//=========================================================================================================================
+void Sentido_AB(){
+   if (digitalRead(sensor1)==HIGH && j==0){ //O carro passou pelo sensor
+        i=0;
+        chave1();
+  }   
+  if (digitalRead(sensor1)==HIGH && j==1){ //Quando o segundo ou demais carros passa pelo sensor
+  tempoInterno=0;  
 
-int tempo1(){
-	for(int i=0; i<=20000;i=i+200){
-			tempo1=i;
-			delay(300);
-			if(digitalRead(A5)==LOW){
-				flag=false;
-			}
-		
-	}
-	return tempo1;
+   while((tempo>0) || (flag==true)){ 
+      while(digitalRead(sensor2)==LOW ){
+        clearLEDs();
+        pickDigit(1);
+        pickNumber(tempo/x%10);
+        delayMicroseconds(del);
+      
+        clearLEDs();
+        pickDigit(2);
+        pickNumber((tempo/x/10)%10);
+        delayMicroseconds(del);
+        if(tempo>0){
+          tempo--; // incrementa n
+        }
+        tempoInterno+=1; // Tem que ser tempoInterno +1 por que Tempo-- vai decrementar o tempo 1000 por exemplo de um a um
+        if(digitalRead(sensor2)!=LOW){
+          tempo=0;
+          goto para;
+        }
+      }
+      para:
+      flag=false;
+    } 
+    Serial.print("tempo: ");
+    Serial.println(tempo);
+    Serial.print("tempo Interno: ");
+    Serial.println(tempoInterno);
+    tempo= tempoInterno;
+    ch1=LOW;
+    
+  }
 }
-int chaveSensor2(){
-
+//=========================================================================================================================
+//=========================================================================================================================
+// teste logico das chaves  
+void chave1(){
+  while (digitalRead(sensor2)==LOW){
+        Serial.print(n);
+        Serial.print(" - ");
+        Serial.print(i);
+        Serial.println(" Segundos");
+        i++;
+        n = n+200; // Incrementa n
+        delay(1000); //Segundos
+      }
+      ch1=LOW;
+      j=1;
+      tempo += n;
+        //chave2();
 }
-
+void chave1(){
+  while (digitalRead(sensor2)==LOW){
+        Serial.print(n);
+        Serial.print(" - ");
+        Serial.print(i);
+        Serial.println(" Segundos");
+        i++;
+        n = n+200; // Incrementa n
+        delay(1000); //Segundos
+      }
+      ch1=LOW;
+      j=1;
+      tempo += n;
+        //chave2();
+}
+//=========================================================================================================================
+//=========================================================================================================================
 // subrotina para multiplexar os dígitos do display
-void pickDigit(int x)
-{
+void pickDigit(int x){
   digitalWrite(d1, HIGH);// valor HIGH no pino desabilita o dígito
   digitalWrite(d2, HIGH);
   digitalWrite(d3, HIGH);
   digitalWrite(d4, HIGH);
   
-  switch(x) // cria uma estrutura switch, tendo como entrada
+  switch(x){ // cria uma estrutura switch, tendo como entrada
             // o número do display a ser habilitado
-  {
     case 1: 
       digitalWrite(d1, LOW); // colocando LOW no pino, estamos habilitando
                              // o acendimento dos segmentos
@@ -100,12 +148,11 @@ void pickDigit(int x)
       break;
   }
 }
-
+//=========================================================================================================================
+//=========================================================================================================================
 // subrotina para acender um determinado número no display
-int pickNumber(int x)
-{
-  switch(x) // cria uma estrutura switch para seleção
-  {
+int pickNumber(int x){
+  switch(x){ // cria uma estrutura switch para seleção
     default: 
       zero(); // chama rotina para apresentar número 0 no display 
       return 0;
@@ -149,30 +196,28 @@ int pickNumber(int x)
   }
 }
 
-//void dispDec(int x)
-//{
-  //digitalWrite(p, HIGH);
-//}
-
+//=========================================================================================================================
+//=========================================================================================================================
 // subrotina para apagar todos os segmentos
 void clearLEDs()
 {
   digitalWrite(a, LOW); // valor low no pino, apaga segmento
   digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
+  digitalWrite(cc, LOW);
   digitalWrite(d, LOW);
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
   digitalWrite(g, LOW);
   //digitalWrite(p, LOW);
 }
-
+//=========================================================================================================================
+//=========================================================================================================================
 // mostra número sero no display
 void zero()
 {
   digitalWrite(a, HIGH); // valor HIGH no pino, acende segmento
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, HIGH);
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
@@ -183,7 +228,7 @@ void one()
 {
   digitalWrite(a, LOW);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, LOW);
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
@@ -194,7 +239,7 @@ void two()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
-  digitalWrite(c, LOW);
+  digitalWrite(cc, LOW);
   digitalWrite(d, HIGH);
   digitalWrite(e, HIGH);
   digitalWrite(f, LOW);
@@ -205,7 +250,7 @@ void three()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, HIGH);
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
@@ -216,7 +261,7 @@ void four()
 {
   digitalWrite(a, LOW);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, LOW);
   digitalWrite(e, LOW);
   digitalWrite(f, HIGH);
@@ -227,7 +272,7 @@ void five()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, LOW);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, HIGH);
   digitalWrite(e, LOW);
   digitalWrite(f, HIGH);
@@ -238,7 +283,7 @@ void six()
 {
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, HIGH);
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
@@ -249,7 +294,7 @@ void seven()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, LOW);
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
@@ -260,7 +305,7 @@ void eight()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, HIGH);
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
@@ -271,7 +316,7 @@ void nine()
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
+  digitalWrite(cc, HIGH);
   digitalWrite(d, LOW);
   digitalWrite(e, LOW);
   digitalWrite(f, HIGH);
